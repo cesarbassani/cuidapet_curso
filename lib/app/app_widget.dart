5284b726a17bc893e2e153397c225337bcff392a
Cesar Francisco Bassani da Luz <cluz@grupoimagetech.com.br>
2020-06-16 22:24:02 -0400

    Instalando library get

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,15 +1,16 @@
+import 'package:cuidapetcurso/app/core/theme_cuidapet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:get/get.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       navigatorKey: Modular.navigatorKey,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Slidy',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeCuidapet.theme(),
       initialRoute: '/',
       onGenerateRoute: Modular.generateRoute,
     );
